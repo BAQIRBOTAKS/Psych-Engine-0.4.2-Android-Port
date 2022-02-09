1a7b4e6b901116e4352ced949ae0cab73e9058dd
BAQIRBOTAKS <97069681+BAQIRBOTAKS@users.noreply.github.com>
2022-02-09 08:32:00 +0700

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -901,11 +901,11 @@ class PlayState extends MusicBeatState
 		add(iconP2);
 		reloadHealthBarColors();
          
-        var creditTxt:FlxText = new FlxText(4,healthBarBG.y + 21,0,("Port by (BAQIRBOTAKS!) "), 25);
+        var creditTxt:FlxText = new FlxText(4,healthBarBG.y + 18,0,("Port by BAQIRBOTAKS! "), 27);
         creditTxt.scrollFactor.set();
         creditTxt.setFormat("VCR OSD Mono", 24, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
         creditTxt.borderColor = FlxColor.BLACK;
-        creditTxt.borderSize = 4;
+        creditTxt.borderSize = 6;
         creditTxt.borderStyle = FlxTextBorderStyle.OUTLINE;
         add(creditTxt);
          
